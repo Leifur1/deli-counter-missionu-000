3d352c1769c2f1c0191dfc9c109f010e50f76529
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 18:03:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,7 @@
-class deli_counter
-  katz_deli = []
-  def line(katz_deli)
-    if katz_deli == []
-      puts 'The line is currently empty.'
-    end
+
+katz_deli = []
+def line(katz_deli)
+  if katz_deli == []
+    puts 'The line is currently empty.'
   end
 end
