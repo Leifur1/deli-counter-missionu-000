faa35f5dbf9decd1b7fff038780813a9d7fd982f
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 19:59:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
-def line(katz_deli)
-  if katz_deli == []
+def line(line)
+  if line.size === 0
     puts 'The line is currently empty.'
   else
     output 'The line is currently: '
