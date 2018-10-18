184b52b427f4d679911f47a0f4eb185d76857d6d
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 18:10:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,10 @@ def line(katz_deli)
     puts 'The line is currently empty.'
   end
 end
+def take_a_number(katz_deli, name)
+  count = 1
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{count} in line."
+end
+  
+  
