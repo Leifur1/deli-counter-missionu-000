794a664f395eb8c48202393d2181bb7e09c34a85
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 19:58:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,19 @@
-var line = [];
- function currentLine(katzDeli) {
-  for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
-}
+katz_deli = []
+def line(katz_deli)
+  if katz_deli == []
+    puts 'The line is currently empty.'
+  else
+    output 'The line is currently: '
+    count = 0
+    line.each do |i|
+      count+=1
+      output += "#{count}. #{i}"
+      output += " " if count != line.size
+    end
+    puts output
+    output
+  end
+end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
